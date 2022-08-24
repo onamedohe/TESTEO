d51c6d1d9f96983f2f9e1dfb8179848f210bfeb3
Arquitech <61185052+ecrespo66@users.noreply.github.com>
2022-08-24 17:47:41 +0200

    Update robot.py

--- robot/robot.py
+++ robot/robot.py
@@ -31,7 +31,7 @@ class Robot(Bot):
             4. Open Applications
         """
         self.log.trace("start Method")
-        self.element_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        self.element_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
         self.browser = FirefoxBrowser(undetectable=True)
         self.browser.ignore_popups()
         self.browser.open()
